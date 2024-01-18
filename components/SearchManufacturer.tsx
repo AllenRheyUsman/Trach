ee1586f2072a1a984320fd588dbd36ef9a805bb7
Allen <arusman1987@gmail.com>
2024-01-18 21:12:19 +0800

    typo again in manufaccture

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 import { useState,Fragment  } from 'react';
 import Image from 'next/image';
 import { Combobox, Transition } from '@headlessui/react';
-import { SearchManufacturerProps } from '@/types';
+import { SearchManuFacturerProps } from '@/types';
 import { manufacturers } from '@/constants';
 
-const SearchManufacturer = ( {manufacturer, setManuFacturer} : SearchManufacturerProps) => {
+const SearchManufacturer = ( {manufacturer, setManuFacturer} : SearchManuFacturerProps) => {
   const[query, setQuery] = useState ('')
 
   const filteredManufacturers = query === "" 
