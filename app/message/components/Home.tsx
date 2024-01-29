5530df29240fd54056a803c1032a198ac6d99631
Allen <arusman1987@gmail.com>
2024-01-29 22:06:27 +0800

    asdsa

--- app/message/components/Home.tsx
+++ app/message/components/Home.tsx
@@ -8,7 +8,7 @@ const Home = () => {
         <div className='sm:mx-auto sm:w-full sm:max-w-md'>
             <Image src={"/logo-trach.svg"} width={48} height={48} alt=''/>
             <h2 className='mt-6 text-center text-3xl font-bold tracking-tight text-gray-900'>
-                Sign in to yor account
+                Messaging Page is not yet available- I'm currently building the Sign up and Sign in Functionality. 
             </h2>
         </div>
         <AuthForm/>
